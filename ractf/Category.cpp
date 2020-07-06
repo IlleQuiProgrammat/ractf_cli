9c42e774123a6c415db801f92bf4a2416b5832db
Nathan McAvoy <nathanmc158@gmail.com>
2020-07-06 10:37:33 +0100

    Add parse checks

--- ractf/Category.cpp
+++ ractf/Category.cpp
@@ -3,14 +3,18 @@
 #include <cpprest/json.h>
 namespace ractf {
     Category::Category(web::json::value &jsonData) {
-        id = jsonData[U("id")].as_number().to_int32();
-        displayOrder = jsonData[U("display_order")].as_number().to_int32();
-        name = jsonData[U("name")].as_string();
-        description = jsonData[U("description")].as_string();
-        containedType = ContainedType::unknown;
+        try {
+            id = jsonData[U("id")].as_number().to_int32();
+            displayOrder = jsonData[U("display_order")].as_number().to_int32();
+            name = jsonData[U("name")].as_string();
+            description = jsonData[U("description")].as_string();
+            containedType = ContainedType::unknown;
 
-        for (auto &challenge : jsonData[U("challenges")].as_array()) {
-            challenges.emplace_back(challenge);
+            for (auto &challenge : jsonData[U("challenges")].as_array()) {
+                challenges.emplace_back(challenge);
+            }
+        } catch (...) {
+            std::cerr << "Error whilst parsing category data." << std::endl;
         }
     }
 }
--- ractf/Challenge.cpp
+++ ractf/Challenge.cpp
@@ -2,37 +2,41 @@
 
 namespace ractf {
     Challenge::Challenge(web::json::value &jsonData) {
-        challenge_id = jsonData[U("id")].as_number().to_int32();
-        name = jsonData[U("name")].as_string();
-        category = jsonData[U("category")].as_number().to_int32();
-        description = jsonData[U("description")].as_string();
-        std::string chalType = jsonData[U("challenge_type")].as_string();
-        if (chalType == "default") {
-            challengeType = ChallengeType::def;
-        } else {
-            challengeType = ChallengeType::unknown;
-        }
-        solveCount = jsonData[U("solve_count")].as_number().to_int32();
-        author = jsonData[U("author")].as_string();
-        score = jsonData[U("score")].as_number().to_int32();
-        std::string flagt = jsonData[U("flag_type")].as_string();
-        if (flagt == "plaintext") {
-            flagType = FlagType::plaintext;
-        } else {
-            flagType = FlagType::unknown;
-        }
-        for (auto &it : jsonData[U("unlocks")].as_array()) {
-            unlocks.push_back(it.as_number().to_int32());
-        }
-        solved = jsonData[U("solved")].as_bool();
-        unlocked = jsonData[U("unlocked")].as_bool();
-        hidden = jsonData[U("hidden")].as_bool();
-        firstBlood = jsonData[U("first_blood")].as_number().to_int32();
-        firstBloodTeam = jsonData[U("first_blood_name")].as_string();
-        solveCount = jsonData[U("solve_count")].as_number().to_int32();
-        autoUnlock = jsonData[U("auto_unlock")].as_bool();
-        for (auto &it : jsonData[U("files")].as_array()) {
-            files.push_back(it[U("url")].as_string());
+        try {
+            challenge_id = jsonData[U("id")].as_number().to_int32();
+            name = jsonData[U("name")].as_string();
+            category = jsonData[U("category")].as_number().to_int32();
+            description = jsonData[U("description")].as_string();
+            std::string chalType = jsonData[U("challenge_type")].as_string();
+            if (chalType == "default") {
+                challengeType = ChallengeType::def;
+            } else {
+                challengeType = ChallengeType::unknown;
+            }
+            solveCount = jsonData[U("solve_count")].as_number().to_int32();
+            author = jsonData[U("author")].as_string();
+            score = jsonData[U("score")].as_number().to_int32();
+            std::string flagt = jsonData[U("flag_type")].as_string();
+            if (flagt == "plaintext") {
+                flagType = FlagType::plaintext;
+            } else {
+                flagType = FlagType::unknown;
+            }
+            for (auto &it : jsonData[U("unlocks")].as_array()) {
+                unlocks.push_back(it.as_number().to_int32());
+            }
+            solved = jsonData[U("solved")].as_bool();
+            unlocked = jsonData[U("unlocked")].as_bool();
+            hidden = jsonData[U("hidden")].as_bool();
+            firstBlood = jsonData[U("first_blood")].as_number().to_int32();
+            firstBloodTeam = jsonData[U("first_blood_name")].as_string();
+            solveCount = jsonData[U("solve_count")].as_number().to_int32();
+            autoUnlock = jsonData[U("auto_unlock")].as_bool();
+            for (auto &it : jsonData[U("files")].as_array()) {
+                files.push_back(it[U("url")].as_string());
+            }
+        } catch (...) {
+            std::cerr << "Error whilst parsing challenge data" << std::endl;
         }
     }
 }
